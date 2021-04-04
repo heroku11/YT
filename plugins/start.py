028e9a18b3ff003c203ce9e550e7d74a2f14cba9
heroku11 <81696654+heroku11@users.noreply.github.com>
2021-04-04 09:25:11 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,13 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Developer",url="https://t.me/aryanvikash")],
+        [InlineKeyboardButton("Master",url="http://t.me/Xpras_id")],
         [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Report Bug",url="http://t.me/Xpras_id")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Or Report Bug", url="https://t.me/aryanvikash")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hallo <b>{message.from_user.first_name}</b>\n/help untuk informasi bantuan lainnya"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
