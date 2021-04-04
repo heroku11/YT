9fc784bb9a91560fb630b508eec0b5d8490bb9e1
heroku11 <81696654+heroku11@users.noreply.github.com>
2021-04-04 09:59:27 +0700

    Update

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -7,7 +7,7 @@ import wget
 import os
 from PIL import Image
 
-ytregex = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
+ytregex = r"^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$"
 
 
 @Client.on_message(Filters.regex(ytregex))
