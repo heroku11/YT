bc203299e8b98481feeb5e09213669539b9afb86
heroku11 <81696654+heroku11@users.noreply.github.com>
2021-04-04 09:28:09 +0700

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"Hanya bisa digunakan untuk 1 video (Bukan Untuk Playlist video) Pastikan untuk menyertakan url / link youtube video"
     await message.reply_text(helptxt)
